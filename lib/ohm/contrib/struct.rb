31f4039890bba565a621dce8f96b108c02202d75
Phil Stewart <phil.stewart@lichp.co.uk>
2011-05-13 10:53:00 +0100

    Fixed Ohm::Struct so that changes to struct instances feed back to the underlying attributes

--- lib/ohm/contrib/struct.rb
+++ lib/ohm/contrib/struct.rb
@@ -48,22 +48,39 @@ module Ohm
       def struct(name, struct_class)
         struct_class = Ohm::Model::Wrapper.wrap(struct_class)
 
+        # Struct attribute reader
         define_method(name) do
-          instance = struct_class.unwrap.new
-          instance.members.each do |field|
-            instance[field] = send(:"#{name}_#{struct_class.unwrap}_#{field}")
+          struct_instance = struct_class.unwrap.new
+          model_instance = self
+          struct_instance.members.each do |field|
+            attr = send(:"#{name}_attr", field)
+            struct_instance[field] = send(attr)
+            # Override the field assignment method in the struct instance to
+            # provide a callback to the corresponding attribute writer
+            struct_instance.define_singleton_method(:"#{field}=") do |value|              
+              super(value)
+              model_instance.send(:"#{attr}=", value)
+            end
           end
-          instance
+          struct_instance
         end
         
+        # Struct attribute writer
         define_method(:"#{name}=") do |value|
           if value.kind_of?(struct_class.unwrap)
             value.members.each do |field|
-              attr = :"#{name}_#{struct_class.unwrap}_#{field}"
-              self.class.class_eval("attribute(:#{attr})") unless attributes.include?(attr)
+              attr = send(:"#{name}_attr", field)
               send(:"#{attr}=", value[field])
             end
           end
+        end
+
+        # Returns the attribute name corresponding to the passed field, and
+        # ensures the attribute is defined
+        define_method(:"#{name}_attr") do |field|
+          attr = :"#{name}_#{struct_class.unwrap}_#{field}"
+          self.class.class_eval("attribute(:#{attr})") unless attributes.include?(attr)
+          attr
         end
 
       end
--- test/struct_test.rb
+++ test/struct_test.rb
@@ -1,12 +1,14 @@
 # encoding: UTF-8
 
 require File.expand_path("./helper", File.dirname(__FILE__))
+require 'pry'
 
 Contact = Struct.new(:phone, :email)
 
 class User < Ohm::Model
   include Ohm::Struct
   struct :contact, Contact
+  struct :undeclared, Contact
 end
 
 user = User.create()
@@ -21,9 +23,20 @@ test "read struct data" do
   assert user.contact_Contact_email == user.contact.email
 end
 
+test "write struct data" do
+  user.contact.phone = "1357"
+  assert user.contact.phone == "1357"
+end
+
 test "attribute writers" do
   user.contact_Contact_phone = "5678"
   assert Contact.new("5678", "bob@wherever.blah") == user.contact
   user.contact_Contact_email = "bob@newemail.fneh"
   assert Contact.new("5678", "bob@newemail.fneh") == user.contact
+end
+
+test "use struct without explicitly instantiating it" do
+  assert user.undeclared == Contact.new(nil, nil)
+  user.undeclared.phone = "9876"
+  assert user.undeclared.phone == "9876"
 end
